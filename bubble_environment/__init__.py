82cdf1dc6b6128646b3f9edb6e1071b71964866c
Peng Shi <pengshi@seu.edu.cn>
2020-06-29 08:35:26 +0800

    first commit

--- bubble_environment/__init__.py
+++ bubble_environment/__init__.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
-#
 """
 module introduction
  
-Authors: shipeng(shipeng04@baidu.com)
+Authors: shipeng
 Date:    2020/6/22 下午10:45
 """
